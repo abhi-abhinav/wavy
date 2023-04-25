c81e7cde12bbca121bf536c8ad58f8f409a0b11d
abhi-abhinav <acyadav805@gmail.com>
2023-04-25 16:49:15 +0530

    Updated textfield ui

--- lib/widgets_common/custom_textfield.dart
+++ lib/widgets_common/custom_textfield.dart
@@ -1,21 +1,22 @@
-import 'package:flutter/material.dart';
 import 'package:wavy/consts/consts.dart';
 
-Widget customTextField({String? title, String? hint, controller}) {
+Widget customTextField({String? title, String? hint, controller, isPass}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
       title!.text.color(redColor).fontFamily(semibold).size(16).make(),
       5.heightBox,
       TextFormField(
+        obscureText: isPass,
+        controller: controller,
         decoration: InputDecoration(
             hintStyle: const TextStyle(
               fontFamily: semibold,
-              color: textfieldGrey,
+              color: Colors.black26,
             ),
             hintText: hint,
             isDense: true,
-            fillColor: lightGrey,
+            fillColor: whiteColor,
             filled: true,
             border: InputBorder.none,
             focusedBorder: const OutlineInputBorder(
