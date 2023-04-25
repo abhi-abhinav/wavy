23831567fbcd6fc9813eaf844732002430b9d463
adwxith <21c05.adwaith@sjec.ac.in>
2023-04-25 16:12:26 +0530

    added services to get users uid

---
+++ lib/services/firestore_services.dart
@@ -0,0 +1,13 @@
+import 'package:wavy/consts/consts.dart';
+
+import '../consts/firebase_consts.dart';
+
+class FirestoreServices {
+  //get users data
+  static getUser(uid) {
+    return firestore
+        .collection(usersCollection)
+        .where('id', isEqualTo: uid)
+        .snapshots();
+  }
+}
