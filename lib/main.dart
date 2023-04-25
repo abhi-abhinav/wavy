d6fbdd2d46bde561c66d17e8f514a68c7f4e1305
abhi-abhinav <acyadav805@gmail.com>
2023-04-25 12:49:58 +0530

    Updated main to include splash screen when the app opens

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,30 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'consts/consts.dart';
+import 'package:wavy/consts/consts.dart';
+import 'package:wavy/views/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: appname,
-      theme: ThemeData(),
+    // using getX so
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hack To Future',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.transparent,
+        appBarTheme: const AppBarTheme(
+            // to set appBar icons color
+            iconTheme: IconThemeData(color: golden),
+            backgroundColor: Colors.transparent),
+        fontFamily: regular,
+      ),
+      home: const SplashScreen(),
     );
   }
 }
