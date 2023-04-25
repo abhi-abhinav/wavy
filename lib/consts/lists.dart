5c0c3a5706fb9e9e23f32274836737cde07d19db
abhi-abhinav <acyadav805@gmail.com>
2023-04-25 16:56:42 +0530

    Updated list for sliders, category, images and buttons

--- lib/consts/lists.dart
+++ lib/consts/lists.dart
@@ -1,3 +1,49 @@
-import 'package:wavy/consts/images.dart';
+import 'package:wavy/consts/consts.dart';
 
 const socialIconList = [icGoogleLogo, icFacebookLogo, icTwitterLogo];
+
+// swiper list
+const slidersList = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
+const secondSlidersList = [imgSs1, imgSs2, imgSs3, imgSs4];
+
+// Featured
+const featuredImages1 = [imgS1, imgS2, imgS3];
+const featuredImages2 = [imgS4, imgS5, imgS6];
+
+const featuredTitles1 = [womenDress, girlsDress, girlsWatches];
+const featuredTitles2 = [boysGlasses, mobilePhone, tshirts];
+
+const categoriesList = [
+  womenDress,
+  menClothingFashion,
+  compAccess,
+  automobile,
+  kidtoys,
+  sports,
+  jewelery,
+  cellphone,
+  furniture
+];
+
+const categoryImages = [
+  imgFc1,
+  imgFc2,
+  imgFc3,
+  imgFc4,
+  imgFc5,
+  imgFc6,
+  imgFc7,
+  imgFc8,
+  imgFc9
+];
+
+const itemDetailButtonsList = [
+  video,
+  reviews,
+  privacyPolicy,
+  returnpolicy,
+  supportpolicy
+];
+
+const profileButtonsList = [orders, wishlist, message];
+const profileButtonsIcon = [icFlashDeal, icFlashDeal, icTodaysDeal];
