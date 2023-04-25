71b75fc5c76d8887e354826daae93f26ac4929c9
abhi-abhinav <acyadav805@gmail.com>
2023-04-25 16:57:28 +0530

    Updated strings for home,category,item details and profile

--- lib/consts/strings.dart
+++ lib/consts/strings.dart
@@ -1,9 +1,11 @@
-const appname = "Wavy";
+const appname = "App";
 const appversion = "Version 1.0.0";
-const credits = "Wavers";
+const credits = "Abhinav Adwaith Ciana";
 const email = "Email";
 const emailhint = "admin@admin.com";
 const password = "Password";
+const loggedin = 'Logged in successfully';
+const loggedout = 'Logged out successfully';
 const passwordhint = "*******";
 const reTypePassword = "Retype Password";
 const name = "Name";
@@ -16,3 +18,47 @@ const loginWith = "Login With";
 const privacyPolicy = "Privacy Policy";
 const termsAndCond = "Terms and Conditions";
 const alreadyHaveAnAccount = "Already have an account?";
+
+// home screen strings
+const searchanything = "Seacrh anything...",
+    todayDeal = "Today's Deal",
+    flashSale = "Flash Sale",
+    topSellers = "Top Sellers",
+    brand = "Brand",
+    topCategories = "Top Categories",
+    womenDress = "Women Dress",
+    girlsWatches = "Girls Watches",
+    mobilePhone = "Mobile Phone",
+    boysGlasses = "Boys Glasses",
+    tshirts = "TShirts",
+    girlsDress = "Girls Dresses",
+    featuredProducts = "Featured Products",
+    featuredCategories = "Featured Categories",
+    categories = "Categories";
+
+// category strings
+
+const womenClothing = "Women Dress",
+    menClothingFashion = "Men Clothing & Fashion",
+    compAccess = "Computer & Accessories",
+    automobile = "Automobile",
+    kidtoys = "Kids & Toys",
+    sports = "Sports",
+    jewelery = "Jewelery",
+    cellphone = "Cellphone & Tab",
+    furniture = "Furniture";
+
+// item details string
+const video = "Video",
+    reviews = "Reviews",
+    sellerpolicy = "Seller Policy",
+    returnpolicy = "Return Policy",
+    supportpolicy = "Support Policy",
+    productsyoumaylike = "Products you may like";
+
+// profile strings
+const wishlist = "My Wishlist",
+    orders = "My orders",
+    message = "Messages",
+    oldpass = "Old password",
+    newpass = "New password";
