34a9a7fff6f2983bc5aac18032c74547cf45b4fc
abhi-abhinav <acyadav805@gmail.com>
2023-04-25 11:33:44 +0530

    Added get and get core as a constant

--- lib/consts/consts.dart
+++ lib/consts/consts.dart
@@ -3,3 +3,5 @@ export './images.dart';
 export './strings.dart';
 export './styles.dart';
 export 'package:velocity_x/velocity_x.dart';
+export 'package:get/get_core/src/get_main.dart';
+export 'package:get/get.dart';
